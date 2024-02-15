b3af8f730ff747f716f7a760ffafe352910f5f20
DESKTOP-GKTTNI9\Deepak <deepakb@centroxy.com>
2024-02-15 14:55:57 +0530

    Add project files.

---
+++ MVCapplicationDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCapplicationDemo.MvcApplication" Language="C#" %>
---
+++ MVCapplicationDemo/Models/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCapplicationDemo.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "Enter Your Name")]
+        [StringLength(20, ErrorMessage = "Name should be less than or equal to twenty characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter Your Address")]
+        [StringLength(20, ErrorMessage = "Address should be less than or equal to twenty characters.")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Enter your PhoneNumber")]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
+        public string Mobileno { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Enter Your Date of Birth.")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [MVCapplicationDemo.Models.CustomValidationAttributeDemo.ValidBirthDate(ErrorMessage = "Birth Date can not be greater than current date")]
+        public DateTime Birthdate { get; set; }
+
+        [Required(ErrorMessage = "Enter Your EmailID")]
+        [RegularExpression(@"^[\w-\._\+%]+@(?:[\w-]+\.)+[\w]{2,6}$", ErrorMessage = "Please enter a valid email address")]
+        public string EmailID { get; set; }
+        public List<Customer> ShowallCustomer { get; set; }
+    }
+}
